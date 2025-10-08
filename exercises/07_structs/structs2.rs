774a3bf42b7ccf33202831b00515ff806b6a093d
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-10-08 20:47:29 +0100

    structs2

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -31,7 +31,16 @@ mod tests {
 
     #[test]
     fn your_order() {
-        let order_template = create_order_template();
+        let your_order = Order {
+            name: String::from("Hacker in Rust"),
+            Order.year,
+            Order.made_by_phone,
+            Order.made_by_mobile,
+            Order.made_by_email,
+            Order.item_number,
+            count: 1,
+        };
+        
 
         // TODO: Create your own order using the update syntax and template above!
         // let your_order =
