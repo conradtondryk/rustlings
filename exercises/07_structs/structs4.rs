8c23e70a2d226a31952444e784d24a030f91975e
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-10-11 15:47:50 +0100

    structs4

---
+++ exercises/07_structs/structs4.rs
@@ -0,0 +1,44 @@
+// structs4.rs
+//
+// This exercise will teach you how to define structs from scratch based on
+// requirements. You'll practice choosing appropriate field types.
+//
+// Execute `rustlings hint structs4` or use the `hint` watch subcommand for a
+// hint.
+
+// TODO: Define a struct called `Book` that has the following fields:
+// - title: a String
+// - author: a String
+// - pages: a u32
+// - available: a bool
+
+struct Book {
+    title: String,
+    author: String,
+    pages: u32,
+    available: bool,
+}
+
+// TODO: Define a tuple struct called `RGB` that holds three u8 values
+// representing red, green, and blue color components.
+
+struct RGB(u8, u8, u8);
+
+// TODO: Define a unit struct called `Marker` (these are useful for generic types)
+struct Marker;
+
+fn main() {
+    // Don't modify this code - it should compile once you've defined the structs above
+    let my_book = Book {
+        title: String::from("The Rust Book"),
+        author: String::from("Steve Klabnik"),
+        pages: 552,
+        available: true,
+    };
+
+    let white = RGB(255, 255, 255);
+    let _marker = Marker;
+
+    println!("Book: {} by {}", my_book.title, my_book.author);
+    println!("RGB: ({}, {}, {})", white.0, white.1, white.2);
+}
